d8d4fec5f151edd0137b323e2be1ca7174d856fe
Amardeilh <amardeilhbenoit@gmail.com>
2018-04-11 23:06:33 +0200

    branch createEditor

--- javascript/createEditor.js
+++ javascript/createEditor.js
@@ -2,7 +2,26 @@
     
     $(window).on('load',function(){
         
+        (function(){
+            
+            body = $("<body></body>").attr("id","loadPageBody");
+            $("html").append(body);
+            
+            title = $("<h1></h1>").text("Chargement");
+            progressBar = $("<div></div>").attr("id","progressBar");
+            paragraphe = $("<p></p>").text("création des modules");
+            
+            container = $("<div></div>");
+            container.append(title);
+            container.append(progressBar);
+            container.append(paragraphe);
+            
+        
+            
+            $("#loadPageBody").append(container);
 
+            
+        })();
         
     });
     
